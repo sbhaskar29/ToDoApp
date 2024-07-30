3492aec9f43cdcfb945c99dee94c40c6ca523676
sbhaskar29 <bhaskarpradeep735170@gmail.com>
2024-07-30 21:36:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const listContainer= document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+// for click to check and uncheck
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// to store the data even after refreshing the page
+function saveData() {
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+//to display the data
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
